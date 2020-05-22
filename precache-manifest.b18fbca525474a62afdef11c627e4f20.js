4ad427e3b9bcdcc16def2be2c4a81a8bbe867f01
ayt1da <61749332+Ayt1da@users.noreply.github.com>
2020-05-23 01:40:18 +0530

    Updates

---
+++ precache-manifest.b18fbca525474a62afdef11c627e4f20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3852f9d7d05d66c2fff2cd6a029ebfc",
+    "url": "/Advice/index.html"
+  },
+  {
+    "revision": "953de74d959c47eafe3c",
+    "url": "/Advice/static/css/main.89833d53.chunk.css"
+  },
+  {
+    "revision": "a8c66fd7eb92adddd71d",
+    "url": "/Advice/static/js/2.4bc7b702.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Advice/static/js/2.4bc7b702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953de74d959c47eafe3c",
+    "url": "/Advice/static/js/main.919ed120.chunk.js"
+  },
+  {
+    "revision": "3f78a58ea435574abcb8",
+    "url": "/Advice/static/js/runtime-main.2c4832ba.js"
+  },
+  {
+    "revision": "0fd51ee369aff04b299ee18022ca680b",
+    "url": "/Advice/static/media/mobile.0fd51ee3.jpg"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/Advice/static/media/wide.91c109fc.jpg"
+  }
+]);
